e04b5772392f59c6d4c0ffc4c7c33781191ef44f
rikachka <rikachka1@gmail.com>
2016-12-14 11:47:28 +0300

    Created dirs for tests

---
+++ tests/type_checker/testfiles/1.java
@@ -0,0 +1,26 @@
+class Factorial {
+    public static void main(String[] a){
+    Fac fac;
+    Fac2 c;
+//    bool b;
+    fac = new Fac();	
+    System.out.println(fac.ComputeFac(10));
+//    b = true;
+    System.out.println(true);
+    }
+}
+
+class Fac extends Fac {
+
+    public int ComputeFac(int num){
+    int num2;
+    int num_aux ;
+    num_aux = 5;
+    num2 = 2;
+    if (num2 < 1)
+        num_aux = 1 ;
+    else 
+        num_aux = num2 * (this.ComputeFac(num2-1)) ;
+    return num_aux;
+    }
+}
