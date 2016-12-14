a67c5eb0e9bbb480ef183019da82404249e1fe43
Ivan Shafran <vanobox07@mail.ru>
2016-12-15 00:39:17 +0300

    fixed 8a

--- tests/type_checker/testfiles/8a.java
+++ tests/type_checker/testfiles/8a.java
@@ -9,6 +9,9 @@ class A {
 		int a;
 		int b;
 		int c;
+		A clazz;
+
+		clazz = new A();
 
 		a = 1;
 		b = 2;
@@ -32,7 +35,7 @@ class A {
 
 		c = 1 + true;
 		c = true + 1;
-		c = getInt() + getInt();
+		c = clazz.getInt() + clazz.getInt();
 
 		return 0;
 	}
