70151e840a0b19b950e531d80793d4f532c41113
Ivan Shafran <vanobox07@mail.ru>
2016-12-14 22:52:51 +0300

    добавил тесты 2a 8 a 9a

---
+++ tests/type_checker/testfiles/2a.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args){
+    	A a;
+    	B b;
+    }
+}
+
+class A {
+	A a;
+	Main main;
+	B b;
+}
+
+class B {
+	B b;
+	Main main;
+	A a;
+}
---
+++ tests/type_checker/testfiles/8a.java
@@ -0,0 +1,35 @@
+class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class A {
+	public int getInt() {
+		int a;
+		int b;
+		int c;
+
+		a = 1;
+		b = 2;
+
+		c = a + b;
+		c = a + 1;
+		c = 1 + b;
+
+		c = a - b;
+		c = a - 1;
+		c = 1 - b;
+
+		c = a * b;
+		c = a * 1;
+		c = 1 * b;
+
+		c = a % b;
+		c = a % 1;
+		c = 1 % b;
+		c = 1 % 0;
+
+		return 0;
+	}
+}
