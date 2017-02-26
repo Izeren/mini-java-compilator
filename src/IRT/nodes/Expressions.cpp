0b65a7e9bdcb579ea0cd9599d544ecf22696706b
Alexander Yushmanov <yushalnik@bk.ru>
2017-02-26 21:06:09 +0300

    Added constructors and getters for expressions

--- src/IRT/nodes/Expressions.cpp
+++ src/IRT/nodes/Expressions.cpp
@@ -11,12 +11,43 @@ IRT::CEseqExpression::~CEseqExpression() {
 
 }
 
+const CStatement *IRT::CEseqExpression::getStatement() const {
+    return statement.get();
+}
+
+const CExpression *IRT::CEseqExpression::getExpression() const {
+    return expression.get();
+}
+
+IRT::CEseqExpression::CEseqExpression( std::unique_ptr<const IRT::CStatement> _statement,
+                                       std::unique_ptr<const IRT::CExpression> _expression ) : statement(
+        std::move( _statement )), expression( std::move( _expression )) {}
+
 void IRT::CBinopExpression::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
 IRT::CBinopExpression::~CBinopExpression() {
 
+}
+
+IRT::CBinopExpression::CBinopExpression( std::unique_ptr<const IRT::CExpression> leftOperand,
+                                         std::unique_ptr<const IRT::CExpression> rightOperand,
+                                         IRT::enums::TOperationType operation ) : leftOperand( std::move( leftOperand )),
+                                                                                 rightOperand(
+                                                                                         std::move( rightOperand )),
+                                                                                 type( operation ) {}
+
+const IRT::CExpression *IRT::CBinopExpression::getLeftOperand() const {
+    return leftOperand.get();
+}
+
+const IRT::CExpression *IRT::CBinopExpression::getRightOperand() const {
+    return rightOperand.get();
+}
+
+IRT::enums::TOperationType IRT::CBinopExpression::getOperation() const {
+    return operation;
 }
 
 void IRT::CConstExpression::Accept( IRT::IVisitor &visitor ) {
@@ -27,12 +58,24 @@ IRT::CConstExpression::~CConstExpression() {
 
 }
 
+IRT::CConstExpression::CConstExpression( int value ) : value( value ) {}
+
+int IRT::CConstExpression::getValue() const {
+    return value;
+}
+
 void IRT::CTempExpression::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
 IRT::CTempExpression::~CTempExpression() {
 
+}
+
+IRT::CTempExpression::CTempExpression( const CTemp &temprorary ) : temprorary( temprorary ) {}
+
+const CTemp &IRT::CTempExpression::getTemprorary() const {
+    return temprorary;
 }
 
 void IRT::CNameExpression::Accept( IRT::IVisitor &visitor ) {
@@ -43,6 +86,12 @@ IRT::CNameExpression::~CNameExpression() {
 
 }
 
+IRT::CNameExpression::CNameExpression( const CLabel &label ) : label( label ) {}
+
+const CLabel &IRT::CNameExpression::getLabel() const {
+    return label;
+}
+
 void IRT::CCallExpression::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
@@ -50,6 +99,18 @@ void IRT::CCallExpression::Accept( IRT::IVisitor &visitor ) {
 IRT::CCallExpression::~CCallExpression() {
 
 }
+
+const IRT::CExpression *IRT::CCallExpression::getFunction() const {
+    return function.get();
+}
+
+const IRT::CExpressionList *IRT::CCallExpression::getArgumetns() const {
+    return argumetns.get();
+}
+
+IRT::CCallExpression::CCallExpression(  std::unique_ptr<const IRT::CExpression> function,
+                                        std::unique_ptr<const IRT::CExpressionList> argumetns ) : function(
+        std::move(function )), argumetns( std::move(argumetns )) {}
 
 void IRT::CMemExpression::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
@@ -59,10 +120,36 @@ IRT::CMemExpression::~CMemExpression() {
 
 }
 
+IRT::CMemExpression::CMemExpression(  std::unique_ptr<const IRT::CExpression> address ) : address( std::move(address )) {}
+
+const IRT::CExpression *IRT::CMemExpression::getAddress() const {
+    return address.get();
+}
+
 void IRT::CExpressionList::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
 IRT::CExpressionList::~CExpressionList() {
 
+}
+
+const std::vector<std::unique_ptr<const IRT::CExpression>> &IRT::CExpressionList::getExpressions() const {
+    return expressions;
+}
+
+IRT::CExpressionList::CExpressionList( const IRT::CExpression *expression ) {
+    Add( expression );
+}
+
+IRT::CExpressionList::CExpressionList( std::unique_ptr<const IRT::CExpression> expression ) {
+    Add( std::move( expression ));
+}
+
+void IRT::CExpressionList::Add( const IRT::CExpression *expression ) {
+    expressions.emplace_back( expression );
+}
+
+void IRT::CExpressionList::Add( std::unique_ptr<const IRT::CExpression> expression ) {
+    expressions.push_back( std::move( expression ));
 }
--- src/IRT/nodes/Expressions.h
+++ src/IRT/nodes/Expressions.h
@@ -7,23 +7,38 @@
 #include <bits/unique_ptr.h>
 #include "INode.h"
 #include "../utils/enums.h"
+#include "../utils/CLabel.h"
+#include "../utils/CTemp.h"
+#include "Statements.h"
+#include <vector>
 
 namespace IRT {
-
+//------------------------------------------------------------------------------------------
     class IExpression : public INode {
     public:
         virtual ~IExpression() {};
+
         virtual void Accept( IVisitor &visitor ) = 0;
     };
 
+//------------------------------------------------------------------------------------------
     class CExpression : public IExpression {
     public:
         virtual ~CExpression() {};
+
         virtual void Accept( IVisitor &visitor ) = 0;
     };
 
+//------------------------------------------------------------------------------------------
     class CEseqExpression : public CExpression {
     public:
+        const CStatement *getStatement() const;
+
+        CEseqExpression( std::unique_ptr<const CStatement> statement,
+                         std::unique_ptr<const CExpression> expression );
+
+        const CExpression *getExpression() const;
+
         virtual void Accept( IVisitor &visitor ) override;
 
         virtual ~CEseqExpression();
@@ -33,21 +48,36 @@ namespace IRT {
         std::unique_ptr<const CExpression> expression;
     };
 
+//------------------------------------------------------------------------------------------
     class CBinopExpression : public CExpression {
     public:
         virtual void Accept( IVisitor &visitor ) override;
+
+        CBinopExpression(  std::unique_ptr<const CExpression> leftOperand,
+                           std::unique_ptr<const CExpression> rightOperand, enums::TOperationType operation );
+
+        const CExpression *getLeftOperand() const;
+
+        const CExpression *getRightOperand() const;
+
+        enums::TOperationType getOperation() const;
 
         virtual ~CBinopExpression();
 
     private:
         std::unique_ptr<const CExpression> leftOperand;
         std::unique_ptr<const CExpression> rightOperand;
-        enums::TOperationType operaion;
+        enums::TOperationType operation;
 
     };
 
+//------------------------------------------------------------------------------------------
     class CConstExpression : public CExpression {
     public:
+        int getValue() const;
+
+        CConstExpression( int value );
+
         virtual void Accept( IVisitor &visitor ) override;
 
         virtual ~CConstExpression();
@@ -56,9 +86,14 @@ namespace IRT {
         int value;
     };
 
+//------------------------------------------------------------------------------------------
     class CTempExpression : public CExpression {
     public:
         virtual void Accept( IVisitor &visitor ) override;
+
+        const CTemp &getTemprorary() const;
+
+        CTempExpression( const CTemp &temprorary );
 
         virtual ~CTempExpression();
 
@@ -66,9 +101,14 @@ namespace IRT {
         CTemp temprorary;
     };
 
+//------------------------------------------------------------------------------------------
     class CNameExpression : public CExpression {
     public:
         virtual void Accept( IVisitor &visitor ) override;
+
+        const CLabel &getLabel() const;
+
+        CNameExpression( const CLabel &label );
 
         virtual ~CNameExpression();
 
@@ -76,8 +116,16 @@ namespace IRT {
         CLabel label;
     };
 
+//------------------------------------------------------------------------------------------
     class CCallExpression : public CExpression {
     public:
+        CCallExpression(  std::unique_ptr<const CExpression> function,
+                          std::unique_ptr<const CExpressionList> argumetns );
+
+        const CExpression *getFunction() const;
+
+        const CExpressionList *getArgumetns() const;
+
         virtual void Accept( IVisitor &visitor ) override;
 
         virtual ~CCallExpression();
@@ -87,8 +135,13 @@ namespace IRT {
         std::unique_ptr<const CExpressionList> argumetns;
     };
 
+//------------------------------------------------------------------------------------------
     class CMemExpression : public CExpression {
     public:
+        CMemExpression(  std::unique_ptr<const CExpression> address );
+
+        const CExpression *getAddress() const;
+
         virtual void Accept( IVisitor &visitor ) override;
 
         virtual ~CMemExpression();
@@ -97,11 +150,27 @@ namespace IRT {
         std::unique_ptr<const CExpression> address;
     };
 
+//------------------------------------------------------------------------------------------
     class CExpressionList : CExpression {
     public:
+        CExpressionList() = default;
+
+        CExpressionList( const CExpression *expression );
+
+        CExpressionList( std::unique_ptr<const CExpression> expression );
+
+        void Add( const CExpression *expression );
+
+        void Add( std::unique_ptr<const CExpression> expression );
+
+        const std::vector<std::unique_ptr<const CExpression>> &getExpressions() const;
+
         virtual void Accept( IVisitor &visitor ) override;
 
         virtual ~CExpressionList();
+
+    private:
+        std::vector<std::unique_ptr<const CExpression>> expressions;
     };
 
 
