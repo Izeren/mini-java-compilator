ec85b6bb5c56a4ac6a9669421b231999afc48535
Alexander Yushmanov <yushalnik@bk.ru>
2017-02-26 20:07:07 +0300

    Updated expression for the IRT

--- src/IRT/nodes/Expressions.cpp
+++ src/IRT/nodes/Expressions.cpp
@@ -1,4 +1,68 @@
 //
 // Created by izeren on 2/19/17.
 //
+#include "Expressions.h"
 
+void IRT::CEseqExpression::Accept( IVisitor &visitor ) {
+    visitor.Visit( *this );
+}
+
+IRT::CEseqExpression::~CEseqExpression() {
+
+}
+
+void IRT::CBinopExpression::Accept( IVisitor &visitor ) {
+    visitor.Visit( *this );
+}
+
+IRT::CBinopExpression::~CBinopExpression() {
+
+}
+
+void IRT::CConstExpression::Accept( IRT::IVisitor &visitor ) {
+    visitor.Visit( *this );
+}
+
+IRT::CConstExpression::~CConstExpression() {
+
+}
+
+void IRT::CTempExpression::Accept( IRT::IVisitor &visitor ) {
+    visitor.Visit( *this );
+}
+
+IRT::CTempExpression::~CTempExpression() {
+
+}
+
+void IRT::CNameExpression::Accept( IRT::IVisitor &visitor ) {
+    visitor.Visit( *this );
+}
+
+IRT::CNameExpression::~CNameExpression() {
+
+}
+
+void IRT::CCallExpression::Accept( IRT::IVisitor &visitor ) {
+    visitor.Visit( *this );
+}
+
+IRT::CCallExpression::~CCallExpression() {
+
+}
+
+void IRT::CMemExpression::Accept( IRT::IVisitor &visitor ) {
+    visitor.Visit( *this );
+}
+
+IRT::CMemExpression::~CMemExpression() {
+
+}
+
+void IRT::CExpressionList::Accept( IRT::IVisitor &visitor ) {
+    visitor.Visit( *this );
+}
+
+IRT::CExpressionList::~CExpressionList() {
+
+}
