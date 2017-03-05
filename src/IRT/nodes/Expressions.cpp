2ca724c2cae5dc60e4decc069dcaedb5a9057e96
Княжев Валерий Александрович <valeriy.knyazhev@yandex.ru>
2017-03-05 16:43:18 +0300

    в Statements и Expressions реализован метод Copy

--- src/IRT/nodes/Expressions.cpp
+++ src/IRT/nodes/Expressions.cpp
@@ -8,134 +8,166 @@ void IRT::CEseqExpression::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-IRT::CEseqExpression::~CEseqExpression() {
+IRT::CEseqExpression::~CEseqExpression( ) {
 
 }
 
-const IRT::CStatement *IRT::CEseqExpression::getStatement() const {
-    return statement.get();
+const IRT::CStatement *IRT::CEseqExpression::getStatement( ) const {
+    return statement.get( );
 }
 
-const IRT::CExpression *IRT::CEseqExpression::getExpression() const {
-    return expression.get();
+const IRT::CExpression *IRT::CEseqExpression::getExpression( ) const {
+    return expression.get( );
 }
 
 IRT::CEseqExpression::CEseqExpression( std::unique_ptr<const IRT::CStatement> _statement,
                                        std::unique_ptr<const IRT::CExpression> _expression ) : statement(
-        std::move( _statement )), expression( std::move( _expression )) {}
+        std::move( _statement )), expression( std::move( _expression )) { }
+
+std::unique_ptr<const IRT::CExpression> IRT::CEseqExpression::Copy( ) const {
+    return std::move(
+            std::unique_ptr<const IRT::CExpression>( new CEseqExpression( statement->Copy( ), expression->Copy( ))));
+}
 
 void IRT::CBinopExpression::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-IRT::CBinopExpression::~CBinopExpression() {
+IRT::CBinopExpression::~CBinopExpression( ) {
 
 }
 
 IRT::CBinopExpression::CBinopExpression( std::unique_ptr<const IRT::CExpression> _leftOperand,
                                          std::unique_ptr<const IRT::CExpression> _rightOperand,
-                                         IRT::enums::TOperationType _operation ) : leftOperand( std::move( _leftOperand )),
-                                                                                 rightOperand(
-                                                                                         std::move( _rightOperand )),
-                                                                                 operation( _operation ) {}
+                                         IRT::enums::TOperationType _operation ) :
+        leftOperand( std::move( _leftOperand )),
+        rightOperand( std::move( _rightOperand )),
+        operation( _operation ) { }
 
-const IRT::CExpression *IRT::CBinopExpression::getLeftOperand() const {
-    return leftOperand.get();
+const IRT::CExpression *IRT::CBinopExpression::getLeftOperand( ) const {
+    return leftOperand.get( );
 }
 
-const IRT::CExpression *IRT::CBinopExpression::getRightOperand() const {
-    return rightOperand.get();
+const IRT::CExpression *IRT::CBinopExpression::getRightOperand( ) const {
+    return rightOperand.get( );
 }
 
-IRT::enums::TOperationType IRT::CBinopExpression::getOperation() const {
+IRT::enums::TOperationType IRT::CBinopExpression::getOperation( ) const {
     return operation;
+}
+
+std::unique_ptr<const IRT::CExpression> IRT::CBinopExpression::Copy( ) const {
+    return std::move( std::unique_ptr<const IRT::CExpression>(
+            new CBinopExpression( leftOperand->Copy( ), rightOperand->Copy( ), operation )));
 }
 
 void IRT::CConstExpression::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-IRT::CConstExpression::~CConstExpression() {
+IRT::CConstExpression::~CConstExpression( ) {
 
 }
 
-IRT::CConstExpression::CConstExpression( int _value ) : value( _value ) {}
+IRT::CConstExpression::CConstExpression( int _value ) : value( _value ) { }
 
-int IRT::CConstExpression::getValue() const {
+int IRT::CConstExpression::getValue( ) const {
     return value;
+}
+
+std::unique_ptr<const IRT::CExpression> IRT::CConstExpression::Copy( ) const {
+    return std::move( std::unique_ptr<const IRT::CExpression>( new CConstExpression( value )));
 }
 
 void IRT::CTempExpression::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-IRT::CTempExpression::~CTempExpression() {
+IRT::CTempExpression::~CTempExpression( ) {
 
 }
 
-IRT::CTempExpression::CTempExpression( const IRT::CTemp &_temprorary ) : temprorary( _temprorary ) {}
+IRT::CTempExpression::CTempExpression( const IRT::CTemp &_temprorary ) : temprorary( _temprorary ) { }
 
-const IRT::CTemp &IRT::CTempExpression::getTemprorary() const {
+const IRT::CTemp &IRT::CTempExpression::getTemprorary( ) const {
     return temprorary;
+}
+
+std::unique_ptr<const IRT::CExpression> IRT::CTempExpression::Copy( ) const {
+    return std::move( std::unique_ptr<const IRT::CExpression>( new CTempExpression( temprorary )));
 }
 
 void IRT::CNameExpression::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-IRT::CNameExpression::~CNameExpression() {
+IRT::CNameExpression::~CNameExpression( ) {
 
 }
 
-IRT::CNameExpression::CNameExpression( const IRT::CLabel &_label ) : label( _label ) {}
+IRT::CNameExpression::CNameExpression( const IRT::CLabel &_label ) : label( _label ) { }
 
-const IRT::CLabel &IRT::CNameExpression::getLabel() const {
+const IRT::CLabel &IRT::CNameExpression::getLabel( ) const {
     return label;
+}
+
+std::unique_ptr<const IRT::CExpression> IRT::CNameExpression::Copy( ) const {
+    return std::move( std::unique_ptr<const IRT::CExpression>( new CNameExpression( label )));
 }
 
 void IRT::CCallExpression::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-IRT::CCallExpression::~CCallExpression() {
+IRT::CCallExpression::~CCallExpression( ) {
 
 }
 
-const IRT::CExpression *IRT::CCallExpression::getFunction() const {
-    return function.get();
+const IRT::CExpression *IRT::CCallExpression::getFunction( ) const {
+    return function.get( );
 }
 
-const IRT::CExpressionList *IRT::CCallExpression::getArgumetns() const {
-    return argumetns.get();
+const IRT::CExpressionList *IRT::CCallExpression::getArgumetns( ) const {
+    return arguments.get( );
 }
 
-IRT::CCallExpression::CCallExpression(  std::unique_ptr<const IRT::CExpression> _function,
-                                        std::unique_ptr<const IRT::CExpressionList> _argumetns ) : function(
-        std::move(_function )), argumetns( std::move(_argumetns )) {}
+IRT::CCallExpression::CCallExpression( std::unique_ptr<const IRT::CExpression> _function,
+                                       std::unique_ptr<const IRT::CExpressionList> _argumetns ) : function(
+        std::move( _function )), arguments( std::move( _argumetns )) { }
+
+std::unique_ptr<const IRT::CExpression> IRT::CCallExpression::Copy( ) const {
+    return std::move(
+            std::unique_ptr<const IRT::CExpression>( new CCallExpression( function->Copy( ), arguments->Copy( ))));
+}
 
 void IRT::CMemExpression::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-IRT::CMemExpression::~CMemExpression() {
+IRT::CMemExpression::~CMemExpression( ) {
 
 }
 
-IRT::CMemExpression::CMemExpression(  std::unique_ptr<const IRT::CExpression> _address ) : address( std::move(_address )) {}
+IRT::CMemExpression::CMemExpression( std::unique_ptr<const IRT::CExpression> _address ) : address(
+        std::move( _address )) { }
 
-const IRT::CExpression *IRT::CMemExpression::getAddress() const {
-    return address.get();
+const IRT::CExpression *IRT::CMemExpression::getAddress( ) const {
+    return address.get( );
+}
+
+std::unique_ptr<const IRT::CExpression> IRT::CMemExpression::Copy( ) const {
+    return std::move( std::unique_ptr<const IRT::CExpression>( new CMemExpression( address->Copy( ))));
 }
 
 void IRT::CExpressionList::Accept( IRT::IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-IRT::CExpressionList::~CExpressionList() {
+IRT::CExpressionList::~CExpressionList( ) {
 
 }
 
-const std::vector<std::unique_ptr<const IRT::CExpression>> &IRT::CExpressionList::getExpressions() const {
+const std::vector<std::unique_ptr<const IRT::CExpression>> &IRT::CExpressionList::getExpressions( ) const {
     return expressions;
 }
 
@@ -153,4 +185,12 @@ void IRT::CExpressionList::Add( const IRT::CExpression *_expression ) {
 
 void IRT::CExpressionList::Add( std::unique_ptr<const IRT::CExpression> _expression ) {
     expressions.push_back( std::move( _expression ));
+}
+
+std::unique_ptr<const IRT::CExpression> IRT::CExpressionList::Copy( ) const {
+    CExpressionList *newList = new CExpressionList( );
+    for ( auto it : expressions ) {
+        newList->Add( std::move( it->Copy( )));
+    }
+    return std::move( std::unique_ptr<const CExpression>( newList ));
 }
--- src/IRT/nodes/Statements.cpp
+++ src/IRT/nodes/Statements.cpp
@@ -8,43 +8,55 @@ using namespace IRT;
 CMoveStatement::CMoveStatement( std::unique_ptr<const CExpression> _target,
                                 std::unique_ptr<const CExpression> _source )
         : target( std::move( _target )),
-          source( std::move( _source )) {}
+          source( std::move( _source )) { }
 
-const CExpression *CMoveStatement::Target() const {
-    return target.get();
+const CExpression *CMoveStatement::Target( ) const {
+    return target.get( );
 }
 
-const CExpression *CMoveStatement::Source() const {
-    return source.get();
+const CExpression *CMoveStatement::Source( ) const {
+    return source.get( );
 }
 
 void CMoveStatement::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
+std::unique_ptr<const CStatement> CMoveStatement::Copy( ) const {
+    return std::move( std::unique_ptr<const CStatement>( new CMoveStatement( target->Copy( ), source->Copy( ))));
+}
+
 // ********************************************************************************
 
 CExpStatement::CExpStatement( std::unique_ptr<const CExpression> _expression ) : expression(
-        std::move( _expression )) {}
+        std::move( _expression )) { }
 
-const CExpression *CExpStatement::Expression() const {
-    return expression.get();
+const CExpression *CExpStatement::Expression( ) const {
+    return expression.get( );
 }
 
 void CExpStatement::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
+std::unique_ptr<const CStatement> CExpStatement::Copy( ) const {
+    return std::move( std::unique_ptr<const CStatement>( new CExpStatement( expression->Copy( ))));
+}
+
 // ********************************************************************************
 
-CJumpStatement::CJumpStatement( CLabel _target ) : target( _target ) {}
+CJumpStatement::CJumpStatement( CLabel _target ) : target( _target ) { }
 
-CLabel CJumpStatement::Target() const {
+CLabel CJumpStatement::Target( ) const {
     return target;
 }
 
 void CJumpStatement::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
+}
+
+std::unique_ptr<const CStatement> CJumpStatement::Copy( ) const {
+    return std::move( std::unique_ptr<const CStatement>( new CJumpStatement( target )));
 }
 
 // ********************************************************************************
@@ -56,54 +68,67 @@ CJumpConditionalStatement::CJumpConditionalStatement( TLogicOperator _operation,
                                                       std::unique_ptr<const CLabelStatement> _labelFalse )
         : operation( _operation ),
           leftOperand( std::move( left )), rightOperand( std::move( right )),
-          labelTrue( std::move( _labelTrue )), labelFalse( std::move( _labelFalse )) {}
+          labelTrue( std::move( _labelTrue )), labelFalse( std::move( _labelFalse )) { }
 
-const CExpression *CJumpConditionalStatement::LeftOperand() const {
-    return leftOperand.get();
+const CExpression *CJumpConditionalStatement::LeftOperand( ) const {
+    return leftOperand.get( );
 }
 
-const CExpression *CJumpConditionalStatement::RightOperand() const {
-    return rightOperand.get();
+const CExpression *CJumpConditionalStatement::RightOperand( ) const {
+    return rightOperand.get( );
 }
 
-const CLabelStatement *CJumpConditionalStatement::TrueLabel() const {
-    return labelTrue.get();
+const CLabelStatement *CJumpConditionalStatement::TrueLabel( ) const {
+    return labelTrue.get( );
 }
 
-const CLabelStatement *CJumpConditionalStatement::FalseLabel() const {
-    return labelFalse.get();
+const CLabelStatement *CJumpConditionalStatement::FalseLabel( ) const {
+    return labelFalse.get( );
 }
 
 void CJumpConditionalStatement::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
-TLogicOperator CJumpConditionalStatement::Operation() {
+TLogicOperator CJumpConditionalStatement::Operation( ) {
     return operation;
+}
+
+std::unique_ptr<const CStatement> CJumpConditionalStatement::Copy( ) const {
+    return std::move( std::unique_ptr<const CStatement>(
+            new CJumpConditionalStatement( operation, leftOperand->Copy( ),
+                                           rightOperand->Copy( ),
+                                           std::unique_ptr( labelTrue->Copy( )),
+                                           std::unique_ptr( labelFalse->Copy( )))));
 }
 
 // ********************************************************************************
 
 CSeqStatement::CSeqStatement( std::unique_ptr<const CStatement> _left, std::unique_ptr<const CStatement> _right )
-        : leftStatement( std::move( _left )), rightStatement( std::move( _right )) {}
+        : leftStatement( std::move( _left )), rightStatement( std::move( _right )) { }
 
-const CStatement *CSeqStatement::LeftStatement() const {
-    return leftStatement.get();
+const CStatement *CSeqStatement::LeftStatement( ) const {
+    return leftStatement.get( );
 }
 
-const CStatement *CSeqStatement::RightStatement() const {
-    return rightStatement.get();
+const CStatement *CSeqStatement::RightStatement( ) const {
+    return rightStatement.get( );
 }
 
 void CSeqStatement::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
+std::unique_ptr<const CStatement> CSeqStatement::Copy( ) const {
+    return std::move(
+            std::unique_ptr<const CStatement>( new CSeqStatement( leftStatement->Copy( ), rightStatement->Copy( ))));
+}
+
 // ********************************************************************************
 
-CLabelStatement::CLabelStatement( CLabel _label ) : label( _label ) {}
+CLabelStatement::CLabelStatement( CLabel _label ) : label( _label ) { }
 
-CLabel CLabelStatement::Label() const {
+CLabel CLabelStatement::Label( ) const {
     return label;
 }
 
@@ -111,9 +136,13 @@ void CLabelStatement::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
 }
 
+std::unique_ptr<const CStatement> CLabelStatement::Copy( ) const {
+    return std::move( std::unique_ptr<const CStatement>( new CLabelStatement( label )));
+}
+
 // ********************************************************************************
 
-CStatementList::CStatementList() {}
+CStatementList::CStatementList( ) { }
 
 CStatementList::CStatementList( std::unique_ptr<const CStatement> &statement ) {
     Add( statement );
@@ -123,10 +152,18 @@ void CStatementList::Add( std::unique_ptr<const CStatement> &statement ) {
     statements.push_back( std::move( statement ));
 }
 
-const std::vector<std::unique_ptr<const CStatement>> &CStatementList::Statements() const {
+const std::vector<std::unique_ptr<const CStatement>> &CStatementList::Statements( ) const {
     return statements;
 }
 
 void CStatementList::Accept( IVisitor &visitor ) {
     visitor.Visit( *this );
+}
+
+std::unique_ptr<const CStatement> CStatementList::Copy( ) const {
+    CStatementList *newList = new CStatementList( );
+    for ( auto it :statements ) {
+        newList->Add( std::move( it->Copy( )));
+    }
+    return std::move( std::unique_ptr<const CStatement>( newList ));
 }
