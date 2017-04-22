bfd5433a1bbd8df108bd866ec0f9b1ba836510f8
Ivan Shafran <vanobox07@mail.ru>
2017-04-22 18:56:06 +0300

    изменил тестовый файл BinarySearch

--- tests/IRT_builder/testfiles/BinarySearch.java
+++ tests/IRT_builder/testfiles/BinarySearch.java
@@ -1,8 +1,6 @@
 class BinarySearch{
     public static void main(String[] a){
-	BS bs;
-	bs = new BS();
-	System.out.println(bs.Start(20));
+	System.out.println(new BS().Start(20));
     }
 }
 // This class contains an array of integers and
